7997c21f5480c2a66097d92fa396a1721d22608a
William Bushey <wbushey@gmail.com>
2014-06-01 19:15:44 -0500

    Documented the Microsoft fetchTranslation function Converted fetchTranslation to use a single object for multiple parameters

--- nodejs/utils/microsoft-fetchTranslation.js
+++ nodejs/utils/microsoft-fetchTranslation.js
@@ -1,3 +1,21 @@
-exports.fetchTranslation = function(msg, from_lang, to_lang, medium){
+/**
+ * Requests that the Microsoft translation service translate the provided
+ * message into the requested language and medium. This function handles
+ * authenticating with the Microsoft translation service, making the request
+ * for translation, and receiving the results of translation.
+ *
+ * When using the "text" output medium, this function will return the
+ * translation as a string containing the actual translation.
+ * When using the "audio" output medium, this function will return the URL to
+ * an audio stream of the translation.
+ *
+ * @param msg {String} A string to get a translation of
+ * @param from_lang {String} Language code of the language that msg is currently in
+ * @param to_lang {String} Language code of the language to translate in to
+ * @param medium {String} "text" or "audio", determines which of these media to translate to
+ * @returns {String} Either the text of the translation, or a URL to the audio of the translation,
+ *                   depending on the value of medium
+ */
+exports.fetchTranslation = function(params){
   return 0;
 };
