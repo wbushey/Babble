a2e1b52bcf39ae6ede952377faa4b4d7a4ccb447
Bill Bushey <wbushey@gmail.com>
2014-06-06 11:38:38 -0500

    Rewrote 'join' test to test in a 3 person situation, and be a bit cleaner

--- nodejs/test/test-room.js
+++ nodejs/test/test-room.js
@@ -30,7 +30,7 @@ for (var i = 0; i < 5; i++){
   var new_io_client = add_output(new io_client.connect(serverURL, conn_options), i);
   io_clients.push(new_io_client);
 }
-var langs = ['en', 'es', 'pt', 'fr', 'ja'];
+var langs = ['en', 'es', 'fr', 'pt', 'ja'];
 
 describe('room', function(){
   describe('join', function(){
@@ -42,24 +42,63 @@ describe('room', function(){
                   "Client 0 should get a message indicating that Client 1 has joined.");
 
       var l = room.clients.size(); 
-      var check_joins = function(data){
-        if (io_clients[0].emitted && io_clients[1].emitted){ 
-          expect(room.clients.size()).to.equal(l + 2);
-          expect(io_clients[0].emitted.text).to.equal('1 has joined');
-          expect(io_clients[1].emitted.text).to.equal('1 se ha unido a');
+
+      var check_join2 = function(data){
+        // Check Conditions
+        if (io_clients[0].emitted && io_clients[1].emitted && io_clients[2].emitted){ 
+          expect(room.clients.size()).to.equal(l + 1);
+          l++;
+          expect(io_clients[0].emitted.text).to.equal('2 has joined');
+          expect(io_clients[1].emitted.text).to.equal('2 se ha unido a');
+          expect(io_clients[2].emitted.text).to.equal('2 a rejoint');
           done();
         }
+      }
+
+      var check_join1 = function(data){
+        // Check Conditions
+        if (io_clients[0].emitted && io_clients[1].emitted){ 
+          expect(room.clients.size()).to.equal(l + 1);
+          l++;
+          expect(io_clients[0].emitted.text).to.equal('1 has joined');
+          expect(io_clients[1].emitted.text).to.equal('1 se ha unido a');
+
+        // Reset
+        io_clients[0].removeListener('join', check_join1);
+        io_clients[0].emitted = null;
+        io_clients[1].removeListener('join', check_join1);
+        io_clients[1].emitted = null;
+
+        // Add new listner
+        io_clients[0].on('join', check_join2);
+        io_clients[1].on('join', check_join2);
+        io_clients[2].on('join', check_join2);
+
+        // Start a new join
+        io_clients[2].emit('join', {name: io_clients[2].name, from_lang: langs[2], to_lang: langs[2], output_media: ['text']}); 
+        }
       };
-      io_clients[0].on('join', check_joins);
-      io_clients[1].on('join', check_joins);
-      io_clients[0].on('join', function(){
-        if (!io_clients[1].joined){
-          io_clients[1].emit('join', {name: io_clients[1].name, from_lang: langs[1], to_lang: langs[1], output_media: ['text']});
-          io_clients[1].joined = true;
-        };
-      });
+      var check_join0 = function(data){
+        // Check Condition
+        expect(room.clients.size()).to.equal(l + 1);
+        l++;
+        expect(io_clients[0].emitted.text).to.equal('0 has joined');
+
+        // Reset
+        io_clients[0].removeListener('join', check_join0);
+        io_clients[0].emitted = null;
+
+        // Add new listner
+        io_clients[0].on('join', check_join1);
+        io_clients[1].on('join', check_join1);
+
+        // Start a new join
+        io_clients[1].emit('join', {name: io_clients[1].name, from_lang: langs[1], to_lang: langs[1], output_media: ['text']}); 
+      };
+      io_clients[0].on('join', check_join0);
+
+      // Start the joining
       io_clients[0].emit('join', {name: io_clients[0].name, from_lang: langs[0], to_lang: langs[0], output_media: ['text']});
-      io_clients[0].joined = true;
     });
   });
 
