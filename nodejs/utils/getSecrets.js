591f0cffced063e7c988a1ff762dbb4773c9e037
Bill Bushey <wbushey@gmail.com>
2014-06-06 12:02:10 -0500

    Resetting client_secret to what it should be on Travis Removing evil printing of secrets

--- nodejs/utils/getSecrets.js
+++ nodejs/utils/getSecrets.js
@@ -7,15 +7,13 @@
 exports.getSecret = function(secret_name){
   var val = null;
   if (process.env.TRAVIS){
-    console.log(JSON.stringify(process.env, null, 4));
     val = process.env[secret_name];
   } else {
     var secrets = require('./secret.js');
     val = secrets[secret_name];
   }
-  if (val === null){
-    console.log(secret_name + ' is null for some reason');
+  if (!val){
+    console.log(secret_name + ' is not defined for some reason');
   }
-  console.log(secret_name + ': ' + val);
   return val;
 };
