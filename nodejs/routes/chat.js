da66468438ff7f9bc1e00693f71365cd2b441d90
David Radcliffe <dradcliffe@gmail.com>
2014-06-12 22:54:03 -0500

    Content-Type: text/html

--- nodejs/routes/chat.js
+++ nodejs/routes/chat.js
@@ -2,5 +2,6 @@
 var fs = require('fs');
 
 module.exports = function(request, response){
+  response.writeHead(200, {'Content-Type': 'text/html' });
   response.end(fs.readFileSync('chat.html'));
 };
--- nodejs/routes/home.js
+++ nodejs/routes/home.js
@@ -2,5 +2,6 @@
 var fs = require('fs');
 
 exports.get = function(request, response){
+  response.writeHead(200, {'Content-Type': 'text/html' });
   response.end(fs.readFileSync('index.html'));
 };
